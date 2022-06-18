f8859f332ced951e65790d8db8735b90ca085766
mreram <mreram@yahoo.com>
2022-06-18 20:44:24 +0430

    Add LocalDataSource for getting cities data

---
+++ app/src/main/java/com/eram/backbase/datasource/local/LocalDataSource.kt
@@ -0,0 +1,15 @@
+package com.eram.backbase.datasource.local
+
+import android.content.Context
+import com.eram.backbase.R
+import com.eram.backbase.model.City
+import com.google.gson.Gson
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor() {
+
+    fun getCities(context: Context): List<City> {
+        val assetsReader = context.resources.openRawResource(R.raw.cities).bufferedReader()
+        return Gson().newBuilder().create().fromJson(assetsReader, Array<City>::class.java).toList()
+    }
+}
