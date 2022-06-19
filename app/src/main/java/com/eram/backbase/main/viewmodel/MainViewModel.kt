d6a3a00aea7e1e1c40c0d04e82489e12f07d76e8
mreram <mreram@yahoo.com>
2022-06-19 20:10:36 +0430

    Add MainViewModel and Implement the logic of finding items

--- app/src/main/java/com/eram/backbase/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/eram/backbase/main/viewmodel/MainViewModel.kt
@@ -2,6 +2,8 @@ package com.eram.backbase.main.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.eram.backbase.datasource.local.LocalDataSource
 import com.eram.backbase.model.City
@@ -9,6 +11,7 @@ import com.eram.backbase.radix.RadixTree
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.concurrent.locks.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -19,14 +22,27 @@ class MainViewModel @Inject constructor(
 
     private val radixTree = RadixTree<City>()
 
+    private var _displayLiveData = MutableLiveData<List<City>>()
+    val displayLiveData: LiveData<List<City>> = _displayLiveData
+
+    private val reentrantLock = ReentrantLock()
+
     fun onCreated() {
         viewModelScope.launch(Dispatchers.IO) {
-            val cities = localDataSource.getCities(getApplication())
+            reentrantLock.lock()
+            val cities = localDataSource.getCities(getApplication()).sortedBy {
+                it.name + it.country
+            }
+            _displayLiveData.postValue(cities)
             cities.forEach { radixTree[(it.name + ", " + it.country).lowercase()] = it }
+            reentrantLock.unlock()
         }
     }
 
     fun onQuerySubmit(query: String) {
-        radixTree.getEntriesWithPrefix(query.lowercase())
+        if (radixTree.isNotEmpty() && reentrantLock.isLocked.not()) {
+            val filteredList = radixTree.getEntriesWithPrefix(query.lowercase())
+            _displayLiveData.postValue(filteredList.map { it.value })
+        }
     }
 }
